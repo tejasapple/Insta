3984972f3bb8bc97d0d951d6cbd32370054bda52
tejasapple <tejasapple918@gmail.com>
2026-09-08 16:23:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 # CONFIGURATION & LOGGING
 # ==========================================
 load_dotenv()
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("8799223742:AAFV2JLCqjnhsUUONyBEidTQ0PKNFH0u2uM")
 
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN is missing in .env file.")
